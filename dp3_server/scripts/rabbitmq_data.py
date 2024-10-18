0ec271eb5c917bc6ec780ddceef105c3147b8675
Ondrej Sedlacek <ondrej.sedlacek@cesnet.cz>
2024-10-18 14:37:38 +0200

    rabbitmq_data.py: Remove usage of icinga.

--- dp3_server/scripts/rabbitmq_data.py
+++ dp3_server/scripts/rabbitmq_data.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 """
-Script to check RabbitMQ telemetry data.
+Check RabbitMQ telemetry data.
+
+Fetches data from RabbitMQ and prints it as JSON.
 """
-import argparse
 import json
+from collections import defaultdict
 
-import icinga
 import requests
-
-
-def get_age_seconds(timestamp, now):
-    return (now - timestamp).total_seconds()
-
 
 exported_queue_keys = {
     "messages": "total",
@@ -28,38 +24,45 @@ exported_message_stats_keys = {
 }
 
 if __name__ == "__main__":
-    config = icinga.ConfigParser(
-        section={"dp3": ["python", "source_age_script", "cfg_path"]},
-    )
+    config = {
+        "url": "http://localhost:15672",
+        "user": "guest",
+        "password": "guest",
+    }
 
-# Arguments parser
-parser = argparse.ArgumentParser(
-    description="Returns RabbitMQ telemetry data (designed for Icinga)"
-)
-parser.add_argument("--url", default="http://localhost:15672")
-parser.add_argument("--user", default="guest")
-parser.add_argument("--password", default="guest")
-args = parser.parse_args()
+    output_data = {}
 
-# Get data
-response = requests.get(f"{args.url}/api/queues", auth=(args.user, args.password))
-if response.status_code != 200:
-    print(response.text)
-    raise Exception("Failed to get RabbitMQ data")
+    try:
+        response = requests.get(
+            f"{config['url']}/api/queues",
+            auth=(config["user"], config["password"]),
+        )
+        if response.status_code != 200:
+            raise Exception(
+                f"Failed to fetch data from RabbitMQ API. {response.status_code}, {response.text}"
+            )
 
-data = response.json()
-output_data = {}
+        data = response.json()
 
-for queue in data:
-    appname, queue_name = queue["name"].split("worker-", 1)
-    if len(queue_name) <= 2:
-        queue_name += "-main"
-    for key, alias in exported_queue_keys.items():
-        output_data[f"{queue_name}-{alias}"] = queue[key]
+        for queue in data:
+            appname, queue_name = queue["name"].split("worker-", 1)
+            if len(queue_name) <= 2:
+                queue_name += "-main"
 
-    message_details = queue["message_stats"]
-    for key, alias in exported_message_stats_keys.items():
-        output_data[f"{queue_name}-{alias}"] = message_details[key]["rate"]
+            for key, alias in exported_queue_keys.items():
+                value = queue[key]
+                value_alias = f"{queue_name}-{alias}"
+                output_data[f"{value_alias}"] = value
 
-# Print results
-print(json.dumps(output_data, indent=4, default=str))
+            if "message_stats" in queue:
+                message_details = queue["message_stats"]
+            else:
+                message_details = defaultdict(lambda: defaultdict(float))
+
+            for key, alias in exported_message_stats_keys.items():
+                output_data[f"{queue_name}-{alias}"] = message_details[key]["rate"]
+
+    except Exception as e:
+        output_data["error"] = str(e)
+
+    print(json.dumps(output_data, indent=2))
