88a4fa22096528cf826bbee553709fc9582fe612
Dávid Benko <benko@cesnet.cz>
2025-01-29 09:39:41 +0100

    FE: add new logo

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -5,9 +5,15 @@ import { RouterLink } from 'vue-router'
 <template>
   <nav class="navbar bg-body-tertiary">
     <div class="container">
-      <RouterLink :to="{ name: 'home' }" class="navbar-brand">PANDDA</RouterLink>
+      <RouterLink :to="{ name: 'home' }" class="navbar-brand">
+        <img src="@/assets/logo.svg" alt="PANDDA" class="logo">
+      </RouterLink>
     </div>
   </nav>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped>
+.logo {
+  height: 3.5rem;
+}
+</style>
