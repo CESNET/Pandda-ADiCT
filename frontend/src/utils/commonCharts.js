4774ccac6e0e97e0a6b07f6fed27aa00b3ad70d3
Dávid Benko <benko@cesnet.cz>
2025-03-18 17:42:09 +0100

    style(frontend): use camelCase for parameter names

--- frontend/src/utils/commonCharts.js
+++ frontend/src/utils/commonCharts.js
@@ -36,16 +36,17 @@ export const CHART_COMMON_OPTIONS = {
 }
 
 /**
- * Sets chart datetime range if both `dt_from` and `dt_to` are provided
+ * Sets chart datetime range if both `dtFrom` and `dtTo` are provided
  * @param {Object} chartScaleXOptions Options for the X axis
- * @param {String} dt_from Start date and time formatted like "1970-01-01T01:23"
- * @param {String} dt_to End date and time formatted like "1970-01-01T01:23"
+ * @param {String} dtFrom Start date and time formatted like "1970-01-01T01:23"
+ * @param {String} dtTo End date and time formatted like "1970-01-01T01:23"
+ * @returns {undefined}
  */
-export function setChartDatetimeRange(chartScaleXOptions, dt_from, dt_to) {
-  if (dt_from && dt_to) {
+export function setChartDatetimeRange(chartScaleXOptions, dtFrom, dtTo) {
+  if (dtFrom && dtTo) {
     // + 'Z' to treat as UTC
-    chartScaleXOptions.min = new Date(dt_from + 'Z')
-    chartScaleXOptions.max = new Date(dt_to + 'Z')
+    chartScaleXOptions.min = new Date(dtFrom + 'Z')
+    chartScaleXOptions.max = new Date(dtTo + 'Z')
   }
 }
 
